3f82ce34f382b07c4529782c04f4ac02c999010c
Palash <palashsharma359@gmail.com>
2025-03-24 02:10:24 +0530

    checking for cookie

--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ app.use(cors({
     origin: [process.env.CLIENT_URL], // Replace with your frontend origin(s)
     methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'], // Allowed methods
     credentials: true, // Allow cookies to be sent
+    allowedHeaders: ["Content-Type", "Authorization"], // ✅ Explicitly allow headers
+
 }))
 app.use(cookieParser())
 app.use(bodyParser.json({extended:true}));

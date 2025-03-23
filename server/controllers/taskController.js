d3daeb3e9e9f8c2dc4c8838d92e7d4e973224ce6
Palash <palashsharma359@gmail.com>
2025-03-24 02:31:02 +0530

    checking for cookie

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -32,8 +32,8 @@ export const addTask = async (req, res) => {
 
 export const getAllTask = async (req, res) => {
   try {
-      // const { authToken } = req.cookies; // ✅ Extract token from cookies
-      const authToken = req.headers.authorization?.split(" ")[1]s
+      // const { authToken } = req.cookies; // ✅ Extract token fro cookies
+      const authToken = req.headers.authorization?.split(" ")[1]
       console.log(req.headers.authorization)
       // console.log(req.cookies)
       // console.log('&&&&&&&&&&&&',authToken)

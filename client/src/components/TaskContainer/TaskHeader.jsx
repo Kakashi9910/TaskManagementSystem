a8f82b3a557e9d95c4ea6969e1dce1f18274a458
Palash <palashsharma359@gmail.com>
2025-03-24 10:40:11 +0530

    add logout feature

--- client/src/components/TaskContainer/TaskHeader.jsx
+++ client/src/components/TaskContainer/TaskHeader.jsx
@@ -1,6 +1,17 @@
 
+import Cookies from "js-cookie";
+import { useContext } from "react";
+import { AccountContext } from "../../context/AccoutProvider";
+
 const TaskHeader = () => {
-    return <h2>TaskBuddy</h2>
+    const {setAccount} = useContext(AccountContext)
+    console.log(account)
+    return <div className="d-flex justify-content-between">
+         <h2>TaskBuddy</h2>
+         <div>
+         <button type="button" class="btn btn-danger" onClick={()=>{Cookies.remove('authToken'); setAccount('')}}>Log Out</button>
+         </div>
+    </div>
 
 }
 
